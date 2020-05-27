cb541a70a2876ea62946aa61d6d67f906cf6c698
kulinskir <robert.kulinski93@gmail.com>
2020-05-27 22:36:41 +0200

    Language context

--- src/App.js
+++ src/App.js
@@ -8,28 +8,44 @@ import {
   Route,
   Redirect,
 } from "react-router-dom";
+import { LanguageContext } from "./LanguageContext";
 
-function App() {
-  return (
-    <div className="App">
-      <Router>
-        <Switch>
-          <Route exact path="/">
-            <Redirect to="/profile" />
-          </Route>
-          <Route path="/profile">
-            <Profile />
-          </Route>
-          <Route path="/chat-container">
-            <ChatContainer />
-          </Route>
-          <Route path="*">
-            <div>404 Page</div>
-          </Route>
-        </Switch>
-      </Router>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    language: "pl",
+  };
+
+  changeLanguage = () => {
+    this.setState(({ language }) => ({
+      language: language === "pl" ? "en" : "pl",
+    }));
+  };
+
+  render() {
+    return (
+      <LanguageContext.Provider value={this.state.language}>
+        <button onClick={this.changeLanguage}>Switch Language</button>
+        <div className="App">
+          <Router>
+            <Switch>
+              <Route exact path="/">
+                <Redirect to="/profile" />
+              </Route>
+              <Route path="/profile">
+                <Profile />
+              </Route>
+              <Route path="/chat-container">
+                <ChatContainer />
+              </Route>
+              <Route path="*">
+                <div>404 Page</div>
+              </Route>
+            </Switch>
+          </Router>
+        </div>
+      </LanguageContext.Provider>
+    );
+  }
 }
 
 export default App;
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,15 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { LanguageContext } from "../LanguageContext";
 
 class Profile extends React.Component {
   render() {
+    const routeName = this.context === "pl" ? "Czat" : "Chat";
     return (
       <div>
         <h1>Profile</h1>
-        <Link to="chat-container">Chat</Link>
+        <Link to="chat-container">{routeName}</Link>
       </div>
     );
   }
 }
+
+Profile.contextType = LanguageContext;
 
 export default Profile;
