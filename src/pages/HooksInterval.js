fd315dcb163ebcda403889d101c01246456446ce
kulinskir <robert.kulinski93@gmail.com>
2020-05-30 19:01:26 +0200

    hooks interval

--- src/pages/HooksInterval.js
+++ src/pages/HooksInterval.js
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 const HooksInterval = () => {
-  return <div></div>;
+  const [stateValue, setStateValue] = useState(0);
+  const intervalRef = useRef();
+
+  useEffect(() => {
+    intervalRef.current = setInterval(() => {
+      setStateValue((prev) => prev + 1);
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalRef.current);
+    };
+  }, []);
+
+  return (
+    <div>
+      <div>From useState: {stateValue}</div>
+    </div>
+  );
 };
 
 export default HooksInterval;
